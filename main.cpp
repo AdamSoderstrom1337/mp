5370f3f7de99e87228ccc0d556fc743060a279d2
Henrik Nilsson <Henrik Nilsson>
2016-03-10 13:05:45 +0100

    added shader and changed some things in main to implement shader (currently not working as intended)

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#define GLEW_STATIC
+
 #include <GL/glew.h>
 #include <glm/glm.hpp>
 
@@ -10,12 +12,14 @@
 #endif
 
 #include <GLFW/glfw3.h>
+#include <GL/glew.h>
 #include <stdlib.h>
 #include <math.h>
 #include <iostream>
 #include <stdio.h>
 
 #include "Cube.h"
+#include "shading.h"
 
 
 
@@ -37,21 +41,20 @@ static void error_callback(int error, const char* description);
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 void rotCamera();
 
-
 int main(void)
 {
     int option1, option2;
 
     std::cout << "[1] - Rubber" << std::endl
     << "[2] - Jelly"  << std::endl << std::endl;
-    
+
     std::cout << "Enter material 1: ";
     std::cin >> option1;
 
 
     std::cout << "Enter material 2: ";
     std::cin >> option2;
-    
+
     switch (option1) {
         case 1:
             cube1.setConstans(2000, 40); //Rubber
@@ -62,7 +65,7 @@ int main(void)
         default:
             break;
     }
-    
+
     switch (option2) {
         case 1:
             cube2.setConstans(2000, 40); //Rubber
@@ -73,7 +76,7 @@ int main(void)
         default:
             break;
     }
-    
+
     GLFWwindow* window;
     glfwSetErrorCallback(error_callback);
 
@@ -88,15 +91,37 @@ int main(void)
     }
 
     glfwMakeContextCurrent(window);
-    
+
     glClearColor(0.6, 0.9, 1.0, 1.0);
     glLoadIdentity();
-    
-    glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+
+    glPolygonMode(GL_FRONT,GL_FILL);
 
 
     glfwSwapInterval(1);
     glfwSetKeyCallback(window, key_callback);
+
+    GLuint shaderProg = LoadShader("shader.vert", "shader.frag");
+
+    //THIS IS NEW LIGHTING STUFF
+    /*
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    GLfloat amb[] = {0.8, 0.8, 0.8, 1.0};
+    GLfloat dif[] = {0.4, 0.4, 0.4, 1.0};
+    GLfloat spec[] = {0.5, 0.5, 0.5, 1.0};
+    GLfloat pos[] = {0.0, -10.0, 10.0, 1.0};
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, amb);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, dif);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, spec);
+    glLightfv(GL_LIGHT0, GL_POSITION, pos);
+*/
+    glEnable(GL_COLOR_MATERIAL);
+    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+    glColorMaterial(GL_FRONT, GL_SPECULAR);
+
+    //END NEW LIGHTING STUFF
 
     while (!glfwWindowShouldClose(window))
     {
@@ -112,10 +137,11 @@ int main(void)
 
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(shaderProg);
 
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
-        
+
         glEnable(GL_CULL_FACE);
         glEnable(GL_NORMALIZE);
         glEnable(GL_DEPTH_TEST);
@@ -125,62 +151,73 @@ int main(void)
 
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
-        
+
         glCullFace(GL_FRONT);
         glLoadIdentity();
-         
-        
+
         gluLookAt(0.0f, 0.5f, 3.0f,
                   cube1.getCenter().x-0.5f, 0.0f, cube1.getCenter().z,
                   0.0f, 1.0f, 0.0f);
-        
 
+        //GLfloat pos[] = {0.0+cube1.getCenter().x-0.5f, 0.0, 10.0+cube1.getCenter().z, 1.0};
+        //glLightfv(GL_LIGHT0, GL_POSITION, pos);
 
-        
         /* ----------------------- Render code ---------------------------- */
-    
+
+        GLint objColorLocation = glGetUniformLocation(shaderProg, "color");
+
         //Floor
         glPushMatrix();
         glRotatef(90, 1.0f, 0.0f, 0.0f);
         glTranslatef(-8.0f, -8.0f, 1.0f);
-            glBegin(GL_QUADS);
-            for (unsigned int x =0;x<GridSizeX;++x)
-                for (unsigned int y =0;y<GridSizeY;++y)
-                {
-                    if ((x+y)%2) //modulo 2
-                        glColor3f(1.0f,1.0f,1.0f); //white
-                    else
-                        glColor3f(0.0f,0.0f,0.0f); //black
-                    
-                    glVertex2f(    x*SizeX,    y*SizeY);
-                    glVertex2f((x+1)*SizeX,    y*SizeY);
-                    glVertex2f((x+1)*SizeX,(y+1)*SizeY);
-                    glVertex2f(    x*SizeX,(y+1)*SizeY);
-                    
+        for (unsigned int x =0;x<GridSizeX;++x){
+            for (unsigned int y =0;y<GridSizeY;++y){
+                if ((x+y)%2){
+                    if (objColorLocation != -1){
+                       glUniform3f(objColorLocation, 10.0, 10.0, 10.0);   //White tiles
+                    }
                 }
-            glEnd();
+                else{
+                    if (objColorLocation != -1){
+                       glUniform3f(objColorLocation, 0.0, 0.0, 0.0);   //Black tiles
+                    }
+                }
+                glBegin(GL_QUADS);
+                glVertex2f(    x*SizeX,    y*SizeY);
+                glVertex2f((x+1)*SizeX,    y*SizeY);
+                glVertex2f((x+1)*SizeX,(y+1)*SizeY);
+                glVertex2f(    x*SizeX,(y+1)*SizeY);
+                glEnd();
+            }
+        }
         glPopMatrix();
-        
-        glColor3f(0.5f, 0.5f, 0.5f);
+
+
         glPushMatrix();
+            if(objColorLocation != -1){
+                glUniform3f(objColorLocation, 0.5, 0.5, 0.5);
+            }
             glTranslatef(-1.4f, 0, -0.5f);
-            cube1.draw();
-            cube1.transBot(glm::vec3(transX,transY,transZ));
             cube1.update();
+            cube1.transBot(glm::vec3(transX,transY,transZ));
+            cube1.draw();
         glPopMatrix();
-        
-        glColor3f(1.0f, 0.5f, 0.25f);
-            glPushMatrix();
+
+
+        glPushMatrix();
+            if(objColorLocation != -1){
+                glUniform3f(objColorLocation, 1.0, 0.5, 0.25);
+            }
             glTranslatef(0.4f, 0, -0.5f);
             cube2.update();
             cube2.transBot(glm::vec3(transX,transY,transZ));
             cube2.draw();
         glPopMatrix();
-        
-        
-        
+
+
+
         /* ----------------------------------------------------------------- */
-        
+
         glfwSwapBuffers(window);
         glfwPollEvents();
 
@@ -201,7 +238,7 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
     float camStep=2.0f;
     float transStep=0.01f;
-    
+
     switch (key) {
         case GLFW_KEY_ESCAPE:
             glfwSetWindowShouldClose(window, GL_TRUE);
@@ -218,38 +255,38 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
         case GLFW_KEY_RIGHT:
             right+=camStep;
             break;
-            
+
         case GLFW_KEY_BACKSPACE:
             right=0;
             left=0;
             up=0;
             down=0;
             break;
-            
-            
-            
+
+
+
         /* Cube controls */
         case GLFW_KEY_W:
             transZ-=transStep;
             break;
-            
+
         case GLFW_KEY_S:
             transZ+=transStep;
             break;
-            
+
         case GLFW_KEY_A:
             transX-=transStep;
             break;
-        
+
         case GLFW_KEY_D:
             transX+=transStep;
-            
+
             break;
-        
+
         case GLFW_KEY_R:
             cube1.jump();
             break;
-            
+
         case GLFW_KEY_F:
             cube2.jump();
             break;
@@ -277,17 +314,9 @@ void rotCamera(){
     glRotatef(up+down, 1.0f, 0.0f, 0.0f);
     glRotatef(left+right, 0.0f, 1.0f, 0.0f);
      */
-    
+
     glTranslatef(0.0f, 0.0f, -3.0f);
     glRotatef(glfwGetTime()*10, 0.0f, 1.0f, 0.0f);
-    
-    
+
+
 }
-
-
-
-
-
-
-
-
---
+++ shading.cpp
@@ -0,0 +1,95 @@
+#define GLEW_STATIC
+
+#include "shading.h"
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+
+
+std::string readFile(const char *filePath) {
+    std::string content;
+    std::ifstream fileStream(filePath, std::ios::in);
+
+    if(!fileStream.is_open()) {
+        std::cerr << "Could not read file " << filePath << ". File does not exist." << std::endl;
+        return "";
+    }
+
+    std::string line = "";
+    while(!fileStream.eof()) {
+        std::getline(fileStream, line);
+        content.append(line + "\n");
+    }
+
+    fileStream.close();
+    return content;
+}
+
+GLuint LoadShader(const char* vertex_path, const char* fragment_path){
+
+    if (glewInit() != GLEW_OK){
+        exit(EXIT_FAILURE);
+    }
+
+    GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // Read shaders
+    std::string vertShaderStr = readFile(vertex_path);
+    std::string fragShaderStr = readFile(fragment_path);
+    const char *vertShaderPtr = vertShaderStr.c_str();
+    const char *fragShaderPtr = fragShaderStr.c_str();
+
+    GLint result = GL_FALSE;
+    int logLength;
+
+    // Compile vertex shader
+    std::cout << "Compiling vertex shader:" << vertex_path << std::endl;
+    glShaderSource(vertShader, 1, &vertShaderPtr, NULL);
+    glCompileShader(vertShader);
+
+    // Check vertex shader
+    glGetShaderiv(vertShader, GL_COMPILE_STATUS, &result);
+    glGetShaderiv(vertShader, GL_INFO_LOG_LENGTH, &logLength);
+    std::vector<GLchar> vertShaderError((logLength > 1) ? logLength : 1);
+    glGetShaderInfoLog(vertShader, logLength, NULL, &vertShaderError[0]);
+    std::cout << &vertShaderError[0] << std::endl;
+
+    // Compile fragment shader
+    std::cout << "Compiling fragment shader." << std::endl;
+    glShaderSource(fragShader, 1, &fragShaderPtr, NULL);
+    glCompileShader(fragShader);
+
+    // Check fragment shader
+    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &result);
+    glGetShaderiv(fragShader, GL_INFO_LOG_LENGTH, &logLength);
+    std::vector<GLchar> fragShaderError((logLength > 1) ? logLength : 1);
+    glGetShaderInfoLog(fragShader, logLength, NULL, &fragShaderError[0]);
+    std::cout << &fragShaderError[0] << std::endl;
+
+    // Link program
+    std::cout << "Linking program" << std::endl;
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertShader);
+    glAttachShader(program, fragShader);
+    glLinkProgram(program);
+
+    // Check program
+    glGetProgramiv(program, GL_LINK_STATUS, &result);
+    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &logLength);
+    std::vector<char> programError( (logLength > 1) ? logLength : 1 );
+    glGetProgramInfoLog(program, logLength, NULL, &programError[0]);
+    std::cout << &programError[0] << std::endl;
+
+    glDetachShader(program, vertShader);
+    glDetachShader(program, fragShader);
+
+    glDeleteShader(vertShader);
+    glDeleteShader(fragShader);
+
+    return program;
+}
